7e8a443173d77f0b542329f4cc72353d7e3ce727
jaypop <me@jaypop.dev>
2024-03-27 02:08:12 +0000

    moramoraboraborabora

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -1,17 +1,16 @@
 return {
     'nvim-telescope/telescope-file-browser.nvim',
-    dependencies = {'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim'},
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
     config = function()
         require('telescope').setup({
             extensions = {
                 file_browser = {
-                    theme = 'ivy',
-                    -- layout_config = {
-                    -- 	width = 100,
-                    -- 	height = 100,
-                    -- },
+                    theme = 'dropdown',
+                    layout_config = {
+                        width = 100,
+                    },
+                    hidden = true,
                     hijack_netrw = true
-                    -- hidden = true,
                 }
             }
         })
