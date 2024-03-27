8230a356088c89153b6bae336d79f931e077488d
jaypop <me@jaypop.dev>
2024-03-27 01:55:40 +0000

    moramoraborabora

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,17 +1,21 @@
+-- add file browser extention to telescope and keymap
+require('telescope').load_extension 'file_browser'
+
+vim.keymap.set('n', '<leader>f', vim.diagnostic.open_float, { desc = 'Open Telescope File Browser' })
+vim.keymap.set('n', '<space>fb', function()
+	require('telescope').extensions.file_browser.file_browser()
+end, { desc = 'Open [B]rowser' })
+
+
 -- add telescope search modes
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>p', vim.diagnostic.open_float, { desc = 'Project search' })
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = 'Search [F]iles'})
+vim.keymap.set('n', '<leader>pg', builtin.git_files, {desc = 'Search [G]it files'})
+vim.keymap.set('n', '<leader>pr', function()
 	builtin.grep_string({ search = vim.fn.input('Grep > ') })
-end)
+end, {desc = 'Search by G[R]ep'})
 
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-
--- add file browser extention to telescope and keymap
-require('telescope').load_extension 'file_browser'
-vim.keymap.set('n', '<space>fb', function()
-	require('telescope').extensions.file_browser.file_browser()
-end)
+vim.keymap.set('n', '<leader>h', vim.diagnostic.open_float, { desc = 'Help' })
+vim.keymap.set('n', '<leader>ht', builtin.help_tags, {desc = 'Help [T]ags'})
--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -72,9 +72,9 @@ return {
             })
 
             -- vim.cmd('colorscheme rose-pine')
-            -- vim.cmd('colorscheme rose-pine-main')
+            vim.cmd('colorscheme rose-pine-main')
             -- vim.cmd('colorscheme rose-pine-moon')
-            vim.cmd('colorscheme rose-pine-dawn')
+            -- vim.cmd('colorscheme rose-pine-dawn')
         end,
     },
 }
