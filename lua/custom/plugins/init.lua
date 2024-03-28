c4fa92754311dc1840c3451ac0d442301d75214c
jaypop <me@jaypop.dev>
2024-03-28 00:44:38 +0000

    wip

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,5 @@ return {
     { dir = 'plugins.rose-pine' },
     { dir = 'plugins.telescope-file-browser' },
     { dir = 'plugins.neotest' },
+    { dir = 'nvim-dap-vscode-js' },
 }
--- plugin_experiments.lua
+++ plugin_experiments.lua
@@ -79,4 +79,4 @@
 	--		vim.g.db_ui_use_nerd_fonts = 1
 	--	end,
 	--}
-}
+-- }
