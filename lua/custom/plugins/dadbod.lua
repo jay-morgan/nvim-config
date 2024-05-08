b1c31e4d5b2c0407c64b477261cfe98ea6405f16
jaypop <me@jaypop.dev>
2024-05-08 18:10:28 +0100

    db 3

--- lua/custom/plugins/dadbod.lua
+++ lua/custom/plugins/dadbod.lua
@@ -1,6 +1,4 @@
 -- TODO
--- maybe reove UI and just use  DB - get the connection on startup working with env vars and learn connection flow
-
 -- define connections strings somewhere
 -- fix mongo autocomplete within buffer
 -- see if bottom buffer can be new buffer instead, or split right/left
@@ -27,8 +25,8 @@ return {
         vim.g.db_ui_use_nerd_fonts = 1
         vim.g.db_ui_show_help = 1
         vim.g.db_ui_auto_execute_table_helpers = 1
-        vim.g.db_ui_winwidth = 60
-        vim.g.db_ui_default_query = "db.{table}.find({})"
+        vim.g.db_ui_winwidth = 50
+        vim.g.db_ui_default_query = "db.{table}.find({}).sort({updatedAt: -1})"
         vim.g.db_ui_save_location = "~/work/db_ui_queries"
     end,
 }
