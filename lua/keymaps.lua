b6b05a99881a74ee97961c2ef8ec55ed4bc04529
jaypop <me@jaypop.dev>
2024-08-27 00:05:14 +0100

    git messenger

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -49,6 +49,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<leader>gm", function()
+  vim.cmd ":GitMessenger<CR>"
+end, { noremap = true, desc = "[G]it [M]essenger" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
