2648741003b22fcf5abb8296b463325eaeda4a38
jaypop <me@jaypop.dev>
2024-09-02 15:28:15 +0100

    yank path

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,6 +11,13 @@ vim.g.maplocalleader = " "
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- Copy relative file path
+vim.keymap.set("n", "<leader>y", function()
+  local relative_path = vim.fn.fnamemodify(vim.fn.expand "%", ":.")
+  vim.fn.setreg("+", relative_path)
+  print("Copied relative path: " .. relative_path)
+end, { desc = "Yank relative file path" })
+
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
